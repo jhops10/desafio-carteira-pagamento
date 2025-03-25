30e697c4cadd7408cee018943b9fae25a324e7f8
João Paulo <joaowais@gmail.com>
2025-03-24 21:10:16 -0300

    adiciona AuthorizationDTO e DataDTO

---
+++ src/main/java/com/jhops10/desafio_picpay/dtos/AuthorizationDTO.java
@@ -0,0 +1,4 @@
+package com.jhops10.desafio_picpay.dtos;
+
+public record AuthorizationDTO(String status, DataDTO data) {
+}
---
+++ src/main/java/com/jhops10/desafio_picpay/dtos/DataDTO.java
@@ -0,0 +1,4 @@
+package com.jhops10.desafio_picpay.dtos;
+
+public record DataDTO(String authorization) {
+}
